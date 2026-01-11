fd23bd4cee8f8390874b9ccc82ef7874455581a4
Ajay <ajaykumar.ak20052001@gmail.com>
2026-01-11 14:53:53 +0530

    Adding routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { HomePage } from "./pages/home/HomePage";
 import { RegisterLoan } from "./pages/registerPage/RegisterPage";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterLoan />} />
+      </Routes>
     </div>
   );
 }
--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,20 +1,29 @@
+import { Link } from "react-router-dom";
 export function MainHeader() {
   return (
     <header className="bg-white border-b border-gray-200 sticky top-0 z-50 flex justify-between">
       <div className="flex justify-between h-16 items-center p-3">
         <div className="flex items-center gap-3">
           <div className="w-9 h-9 bg-red-600 rounded-xl flex items-center justify-center shadow-sm transform transition hover:scale-105">
-            <span className="text-white font-bold text-xl">A</span>
+            <Link to="/" className="text-white font-bold text-xl">
+              A
+            </Link>
           </div>
-          <h1 className="text-2xl font-bold text-gray-900 tracking-tight">
+          <Link
+            to="/"
+            className="text-2xl font-bold text-gray-900 tracking-tight"
+          >
             Alfa<span className="text-red-600">Money</span>
-          </h1>
+          </Link>
         </div>
       </div>
       <div className="p-3">
-        <button className=" p-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors duration-200">
+        <Link
+          to="/register"
+          className=" p-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors duration-200"
+        >
           Register Loan
-        </button>
+        </Link>
       </div>
     </header>
   );
